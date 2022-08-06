9dd65d76ea0d8180911347536bf1ea55b674bab2
cecilcodespython <107161623+cecilcodespython@users.noreply.github.com>
2022-08-06 23:03:13 +0100

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self,question,correct_answer,choices):
+        self.question=question
+        self.correct_answer = correct_answer
+        self.choices = choices
---
+++ quiz_data.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+parameters = {
+    "amount":20,
+    "type":"multiple"
+}
+
+response = requests.get(url="https://opentdb.com/api.php",params=parameters)
+question_data = response.json()["results"]
